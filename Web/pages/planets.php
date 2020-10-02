315d4ade87f5e959d4c171a85653a4ca2ee5fc14
ahmunnaah <mtmunna82@gmail.com>
2020-10-02 23:39:43 +0600

    n

--- Web/pages/planets.php
+++ Web/pages/planets.php
@@ -76,7 +76,9 @@
         background-repeat: no-repeat;
         background-size: 100% 100%;
         background-attachment: fixed;
+
       }
+
       th,td{
         color: white;
         font-size: 25px;
@@ -160,9 +162,13 @@
 </div>
     </div>
     <div class="w3-container c">
-      <div class="w3-container d">
+      <div class="w3-container w3-half ">
+        
+      </div>
+      <div class="w3-container w3-half ">
         <h2>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32 </h2>
       </div>
+
     </div>
   </body>
 </html>
