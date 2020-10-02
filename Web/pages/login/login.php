6483b6574f440d4eb2def21fd1eca394d988fa30
ahmunnaah <mtmunna82@gmail.com>
2020-10-02 18:54:10 +0600

    n

--- Web/pages/login/login.php
+++ Web/pages/login/login.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
-<?php
-include_once "../nav_bar/menu.php"
-?>
+
 <html>
 <head>
 	<title>Log in</title>
 	<link rel="stylesheet" type="text/css" href="login_style.css">
 </head>
 <body>
+	<div>
+		<?php
+		include_once "../nav_bar/menu.php"
+		?>
+	</div>
+
 	<div class="log">
 		<div class="log_content">
 			<!-- login form -->
@@ -15,14 +19,16 @@ include_once "../nav_bar/menu.php"
 				<h1>Welcome to Space_O!</h1>
 				<p>Don't miss new updates! Sign in to Stay connected</p>
 			</div>
+			<form action="login_handler.php" method="post">
+				<div class="form">
+					<label>Enter your email or phone</label>
+					<input type="text" placeholder="Email or Phone" name="uname">
+					<label>Enter your password</label>
+						<input type="password" placeholder="Password" name="pass">
+						<button type="submit">Sign in</button>
+				 </div>
+			</form>
 
-			<div class="form">
-				<label>Enter your email or phone</label>
-				<input type="text" placeholder="Email or Phone">
-				<label>Enter your password</label>
-			    <input type="password" placeholder="Password">
-			    <button>Sign in</button>
-			 </div>
 
 			<div class="ending">
 				<span><a href="">Forgot Password?</a></span>
