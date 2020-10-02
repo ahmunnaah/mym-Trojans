0739b8ae447c466a0b63795f4291cf747b4e195b
ahmunnaah <mtmunna82@gmail.com>
2020-10-02 19:31:11 +0600

    n

--- Web/pages/Explore/planets.php
+++ Web/pages/Explore/planets.php
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Planets</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   </head>
   <body>
+    <div class="w3-container w3-red">
 
+    </div>
+    <div class="w3-container w3-blue">
+
+    </div>
+    <div class="w3-container w3-green">
+
+    </div>
   </body>
 </html>
