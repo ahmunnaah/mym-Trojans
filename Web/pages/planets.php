0d288906d421956279183e155592e02b1e851a69
ahmunnaah <mtmunna82@gmail.com>
2020-10-03 03:28:49 +0600

    planets page done......

--- Web/pages/planets.php
+++ Web/pages/planets.php
@@ -34,7 +34,8 @@ if ($check > 0) {
         margin-left: 70px;
         background-image: url("../images/mars.png");
         background-repeat: no-repeat;
-        background-size:1400px 1100px;
+        background-size:1450px 1100px;
+        text-align: center;
       }
       .b{
         margin-top: 20px;
@@ -99,7 +100,7 @@ if ($check > 0) {
         font-size: 25px;
         align-items: center;
       }
-      h1,h2{
+      h1,h2,h3{
         color: white;
       }
 
@@ -110,7 +111,7 @@ if ($check > 0) {
       <?php include "nav_bar/menu.php"?>
     </div>
     <div class="w3-container w3-back a">
-      <div class="planet_name">
+      <div class="planet_name w3-center">
         <p>
           <?php
                 echo($row['planet_name']);
@@ -201,7 +202,10 @@ if ($check > 0) {
 
       </div>
       <div class="w3-container w3-half likha">
-        <h2>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32 </h2>
+        <h1>Some Facts!!</h1>
+        <h3><?php
+              echo($row['detail_des']);
+          ?></h3>
       </div>
 
     </div>
